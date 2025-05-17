f72be447c84a69ebe8b9489774f41043cc0c3e89
David Medeiros <davidmedeiros94@hotmail.com>
2025-05-17 12:22:23 -0300

    ajustado a feature e criado arquivo java com os snippets

---
+++ src/test/java/steps/ComprarProduto.java
@@ -0,0 +1,87 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ComprarProduto {
+    @Given("que acesso o site {string}")
+    public void que_acesso_o_site(String string) {
+
+    }
+
+    @When("informo {string} e {string} validos")
+    public void informo_e_validos(String string, String string2) {
+
+    }
+
+    @When("clico no botao Login")
+    public void clico_no_botao_login() {
+
+    }
+
+    @Then("sou redirecionado para a lista de produtos")
+    public void sou_redirecionado_para_a_lista_de_produtos() {
+
+    }
+
+    @When("seleciono o {string}")
+    public void seleciono_o(String string) {
+
+    }
+
+    @Then("sou redirecionado para a pagina do produto")
+    public void sou_redirecionado_para_a_pagina_do_produto() {
+
+    }
+
+    @When("clico no botao Add to Cart")
+    public void clico_no_botao_add_to_cart() {
+
+    }
+
+    @When("clico no botao Cart")
+    public void clico_no_botao_cart() {
+
+    }
+
+    @Then("sou redirecionado para a pagina do carrinho")
+    public void sou_redirecionado_para_a_pagina_do_carrinho() {
+
+    }
+
+    @When("clico no botao Checkout")
+    public void clico_no_botao_checkout() {
+
+    }
+
+    @Then("sou redirecionado para a pagina de checkout")
+    public void sou_redirecionado_para_a_pagina_de_checkout() {
+
+    }
+
+    @When("informo {string}, {string} e {string} nos campos")
+    public void informo_e_nos_campos(String string, String string2, String string3) {
+
+    }
+
+    @When("clico no botao Continue")
+    public void clico_no_botao_continue() {
+
+    }
+
+    @Then("sou redirecionado para a pagina overview")
+    public void sou_redirecionado_para_a_pagina_overview() {
+
+    }
+
+    @When("clico no botao Finish")
+    public void clico_no_botao_finish() {
+
+    }
+
+    @Then("sou redirecionado para a pagina de confirmacao de compra")
+    public void sou_redirecionado_para_a_pagina_de_confirmacao_de_compra() {
+
+    }
+}
--- src/test/resources/features/ComprarProduto.feature
+++ src/test/resources/features/ComprarProduto.feature
@@ -7,22 +7,22 @@ Feature: Comprar produto
     Given que acesso o site "https://www.saucedemo.com/"
     
     When informo "user" e "password" validos
-    And clico no botao "login"
+    And clico no botao Login
     Then sou redirecionado para a lista de produtos
 
     When seleciono o "produto"
     Then sou redirecionado para a pagina do produto
 
-    When clico no botao "add to cart"
-    And clico no botao "cart"
+    When clico no botao Add to Cart
+    And clico no botao Cart
     Then sou redirecionado para a pagina do carrinho
 
-    When clico no botao "checkout"
+    When clico no botao Checkout
     Then sou redirecionado para a pagina de checkout
 
     When informo "primeiro nome", "ultimo nome" e "cep" nos campos
-    And clico no botao "Continue"
+    And clico no botao Continue
     Then sou redirecionado para a pagina overview
 
-    When clico no botao "finish"
+    When clico no botao Finish
     Then sou redirecionado para a pagina de confirmacao de compra
