0fb266c57be0258b49ee50c5ae877aca0df37586
David Medeiros <davidmedeiros94@hotmail.com>
2025-05-17 13:56:08 -0300

    estruturado arquivo BasePage e CommonPage

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class BasePage {
-    
+    public WebDriver driver;
 }
---
+++ src/test/java/pages/CommonPage.java
@@ -0,0 +1,14 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CommonPage {
+    
+    public WebDriver driver;
+
+    // Construtor
+    public CommonPage(WebDriver driver){
+        this.driver = driver;
+
+    }
+}
